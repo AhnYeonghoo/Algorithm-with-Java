b078044aebe3f87b8c3b4d82f1ea637ea4633638
AhnYeonghoo <dksdudgn00@naver.com>
2023-03-27 15:29:57 +0900

    프로그래머스 LV.2 교점에 별 만들기

---
+++ src/programmers/practice/Search.java
@@ -0,0 +1,13 @@
+package programmers.practice;
+
+public class Search {
+
+    private int search(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
