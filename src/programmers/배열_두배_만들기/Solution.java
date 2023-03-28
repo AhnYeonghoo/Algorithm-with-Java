d368ecd8df44685471f2bff85e4045146dee009d
AhnYeonghoo <dksdudgn00@naver.com>
2023-03-28 13:24:04 +0900

    프로그래머스 배열 두배 만들기

---
+++ src/programmers/배열_두배_만들기/Solution.java
@@ -0,0 +1,15 @@
+package programmers.배열_두배_만들기;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            answer[i] += numbers[i] * 2;
+        }
+g
+        return answer;
+    }
+}
